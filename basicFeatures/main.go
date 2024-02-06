5e657aaa3fb4d197433fa532ba9cdbb79511b350
harun961018 <lievander24@gmail.com>
2024-02-06 03:21:37 +0100

    fix: fix go version on go mod file

--- basicFeatures/main.go
+++ basicFeatures/main.go
@@ -5,7 +5,17 @@ import (
 	"math/rand"
 )
 
-func main() {
+func const_example() {
+	const price float32 = 275.00
+	const tax float32 = 27.50
+	fmt.Println(price + tax)
+}
+
+func rand_library_example() {
 	fmt.Println(rand.Int())
 	fmt.Println("value:", rand.Int())
+}
+
+func main() {
+	const_example()
 }
