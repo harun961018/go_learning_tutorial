80e2b41debb101b59c6f6ef68d39da691ec98c6a
harun961018 <lievander24@gmail.com>
2024-02-06 14:54:53 +0100

    feat: Add slice, array, map examples

--- collections/main.go
+++ collections/main.go
@@ -2,42 +2,77 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	// "sort"
 )
 
-func main() {
-	arrayfunc()
-	fmt.Println("===============================")
-	arrayMap()
+//basic array
+func basic_array() {
+	var names [3]string
+	names[0] = "Kayak"
+	names[1] = "Lifejacket"
+	names[2] = "Paddle"
+	fmt.Println(names)
 }
 
-func arrayfunc() {
-	fmt.Println("Hello, Collections")
-	names := [3]string{"Kayak", "Lifejacket", "Paddle"}
-	otherArray := &names
-	names[0] = "Canoe"
-	fmt.Println("names:", names)
-	fmt.Println("otherArray:", otherArray)
-	for index, value := range names {
-		fmt.Println("Index:", index, "Value:", value)
-	}
-
-	names1 := []string{"Kayak", "Lifejacket", "Paddle"}
-	names1 = append(names1, "Hat", "Gloves")
-	fmt.Println(names1)
+//short syntax
+func short_syntax_array_example() {
+	names := [3]string{"Kayak", "top", "jacket"}
+	fmt.Println(names)
 }
 
-func arrayMap() {
-	products := map[string]float64{
-		"kayak":      279,
-		"Lifejacket": 48.95,
-		"Hat":        0,
-	}
+//Comparing array
+func comparing_array_example() {
+	names := [3]string{"bottom", "top", "center"}
+	otherNames := [3]string{"bottom", "top", "center"}
+	same := names == otherNames
+	fmt.Println(same)
+}
 
-	if value, ok := products["Hat"]; ok {
-		fmt.Println("Stored value:", value)
-	} else {
-		fmt.Println("No stored value")
+//Enumerating Array
+func enumerating_array_example() {
+	names := [3]string{"bottom", "top", "center"}
+
+	for index, name := range names {
+		fmt.Println("Index:", index, "Value", name)
+	}
+}
+
+//Basic slice
+func basic_slice_example() {
+	names := make([]string, 3)
+	otherNames := []string{"a", "b", "c"}
+	names[0] = "Kayak"
+	names[1] = "Lifejacket"
+	names[2] = "Paddle"
+	otherNames = append(otherNames, "hat", "hg")
+	anotherNames := append(names, "hat", "gloves")
+	appendNames := append(names, anotherNames...)
+	fmt.Println("names", names)
+	fmt.Println("otherNames", otherNames)
+	fmt.Println("anotherNames", anotherNames)
+	fmt.Println("appendNames", appendNames)
+
+}
+
+//Creating Slices from Existing Arrays
+func creat_slice_array_example() {
+	product := [4]string{"a", "b", "c", "d"}
+	someNames := product[1:3]
+	allNames := product[:]
+	someNames = append(someNames, "d")
+
+	fmt.Println("someNames", someNames)
+	fmt.Println("allNames", allNames)
+}
+
+//Basic Map
+func map_syntax_example() {
+	products := make(map[string]float64, 10)
+	products["Kayak"] = 279
+	products["Lifejacket"] = 48.95
+	otherProdjcts := map[string]float64{
+		"shirt":  100,
+		"jacket": 150,
 	}
 
 	for key, value := range products {
@@ -48,8 +83,14 @@ func arrayMap() {
 	for key, _ := range products {
 		keys = append(keys, key)
 	}
-	sort.Strings(keys)
 	for _, key := range keys {
-		fmt.Println("Key:", key, "Value:", products[key])
+		fmt.Println("otherKey:", key, "value:", products[key])
 	}
+}
+
+func main() {
+	// basic_array()
+	// comparing_array_example()
+	// enumerating_array_example()
+	basic_slice_example()
 }
